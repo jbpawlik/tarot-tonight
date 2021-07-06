1a507c21d31976d06bdadf8318a6ea54eb4fadce
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-06 17:13:29 -0500

    add jquery text method to display info

--- src/main.js
+++ src/main.js
@@ -17,68 +17,36 @@ function getElements(response){
     console.log(item);
     return item;
   }
+  
 
   const array = response.cards;
   const result1 = getRandomItem(array);
   const result2 = getRandomItem(array);
   const result3 = getRandomItem(array);
 
-  // console.log(result);
-  // console.log(result1);
-  // console.log(result2);
+  $("#showCard1").html(`<img src=result1.name_short`);
+  $("#showCard2").html(`<img src=result2.name_short`);
+  $("#showCard3").html(`<img src=result3.name_short`);
 
   $("#showName1").text(result1.name);
   $("#showName2").text(result2.name);
   $("#showName3").text(result3.name);
 
+  $("#showDescription1").text(result1.desc);
+  $("#showDescription2").text(result2.desc);
+  $("#showDescription3").text(result3.desc);
 
-  // console.log(response.cards[0].type);
-  // const key = response.cards['type'];
-  // console.log(key)
+  $("#showMeaning1").text(result1.meaning_up);
+  $("#showMeaning2").text(result2.meaning_up);
+  $("#showMeaning3").text(result3.meaning_up);
 
-  // const majorCards = response.cards;
-  // majorCards.filter(checkMajorCards)
-  // function checkMajorCards(card) {
-  //   if(response.cards.type === "major") {
-  //     console.log(card)
-  //   }
-  // }
-  // let newStack = [];
-  // for(let i=0; i <= response.cards.length; i++) {
-  //   const type = (response.cards[i]['type']);
-  //   console.log(type)
-    
-  //   // console.log(type);
-  //   if (type === "major") {
-  //     newStack.push(response.cards[i]);
-  //     console.log(newStack);
-      
-      // console.log(res)
-      // $('#showName1').text(`${response.cards[0].name}`);
-      // $('#showType1').text(`${response.cards[0].type}`);
-      // $('#showDescription1').text(`${response.cards[0].desc}`);
-    
-      // $('#showName2').text(`${response.cards[1].name}`);
-      // $('#showType2').text(`${response.cards[1].type}`);
-      // $('#showDescription2').text(`${response.cards[1].desc}`);
-    
-      // $('#showName3').text(`${response.cards[2].name}`);
-      // $('#showType3').text(`${response.cards[2].type}`);
-      // $('#showDescription3').text(`${response.cards[2].desc}`);
-    }
+
   
-
-
-
-// $("button#button").click(function(event){
-//   event.preventDefault();
-//   let randomCards = parseInt($("input#userInput").val());
-//   console.log(randomCards);
 
 TarotReading.getTarot()
   .then(function(response){
     getElements(response);
   });
 
-
-  
+meaningStr = result2.meaning_up;
+jobSearchKeyWord = meaningStr.split(", ");
