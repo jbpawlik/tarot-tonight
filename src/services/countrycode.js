ca87ca7a31f374c0e05a09d9db5faf9e0a6e6532
jbpawlik <james.benjamin.pawlik@gmail.com>
2021-07-25 21:14:13 +0000

    Deploying to gh-pages from @ jbpawlik/tarot-tonight@006819394e2c066316472d5a2b6f263f0a93260f 🚀

--- src/services/countrycode.js
+++
@@ -1,9 +0,0 @@
-export default class RestCountries {
-  static async convertCode(countryCode) {
-    return fetch(`https://restcountries.eu/rest/v2/alpha/${countryCode}`)
-      .then(function(response) {
-        return response.json();
-      })
-      .catch(function(error){console.log(error);});
-  }
-}
